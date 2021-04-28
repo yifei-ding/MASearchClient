a215558f44143ecc6af0194d3bcfb08e88c16e8a
Michael <56674316+Michealxh-L@users.noreply.github.com>
2021-04-28 11:49:00 +0200

    Add assignTask

--- searchClient/TaskHandler.java
+++ searchClient/TaskHandler.java
@@ -5,6 +5,7 @@ import domain.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Hashtable;
 
 public class TaskHandler {
     private HashMap<Integer, Task> allTasks = new HashMap<Integer, Task>();
@@ -20,6 +21,95 @@ public class TaskHandler {
     private static final int PRIORITY_SCALING_PARAM = 10;
     public TaskHandler(InMemoryDataSource data) {
         this.data = data;
+    }
+
+    private int getManhattanDistance(Location location1, Location location2){
+        return Math.abs(location1.getCol()- location2.getCol())
+                + Math.abs(location1.getRow())- location2.getRow();
+    };
+
+    public void assignTask2(){
+        allGoals = data.getAllGoals();
+        allAgents = data.getAllAgents();
+        allBoxes = data.getAllBoxes();
+        int taskId = 0;
+//        int goalCount = allGoals.size(); //for setting priority
+        int priority = 0;
+        for (Goal goal : allGoals.values()){
+            //for each goal, find a same name box. Currently use the first matching box.
+            ArrayList<Integer> boxList = data.getBoxByName(goal.getName());
+            if(boxList == null){// No box match, so goal match agent by name ? or + color?
+                //agent to goal without box
+                int goalName = Integer.parseInt(goal.getName());
+                Agent agent = allAgents.get(goalName); // according to goalName match agent
+                priority = getManhattanDistance(agent.getLocation(),goal.getLocation())*10;
+                priority = priority+10000;//considering normally the agent need done box tasks
+                Task task = new Task(taskId,agent.getId(),goal.getLocation(),priority);
+                taskId++;
+                data.addTask(task);
+            }
+            // TODO: box-goal matching
+            else { // if there is any box
+//                Box box = allBoxes.get(boxList.get(0)); //TODO: only one box? improve goal-box matching. Remove box after pairing
+                int min_distance = -1; // match the cloest box
+                Box matchedBox = null; //
+                ArrayList<Integer>  matchAgentIds= new ArrayList<>();
+                for (int boxId : boxList){
+                    Box box = allBoxes.get(boxId);
+                    ArrayList<Integer> agentList = data.getAgentByColor(box.getColor());
+                    if(agentList.size() == 0 || agentList == null){
+                        continue;
+                    }
+                    int distance = getManhattanDistance(goal.getLocation(),box.getLocation());
+                    if (min_distance == -1 || distance < min_distance){
+                        min_distance = distance;
+                        matchedBox = box;
+                        matchAgentIds = agentList;
+                    }
+                }
+                if (matchedBox == null){
+                    System.err.println("This map is not solvable!");
+                    continue;
+                }
+                //then for the box, find a same color agent. Currently use the first matching agent.
+                //TODO: improve box-agent matching
+                int min_score = -1;
+                int min_distance2 = -1;
+                Agent matchedAgent = null;
+                for(int agentId : matchAgentIds){
+                    Agent agent = allAgents.get(agentId);
+                    ArrayList<Integer> tasks = data.getAllTasksByAgent(agentId);
+                    int task_score = 0;
+                    if(tasks != null){
+                        task_score = tasks.size()*10;
+                    }
+                    int distance = getManhattanDistance(agent.getLocation(), matchedBox.getLocation());
+                    int score = distance + task_score;
+                    if (min_score == -1 || score<min_score){
+                        min_score = score;
+                        min_distance2 = distance;
+                        matchedAgent = agent;
+                    }
+                }
+
+                //for each goal, create 2 tasks. Tasks of the first goal has highest priority.
+                //task1 is to find box. Task1 has higher priority than task2.
+//                priority = goalCount*PRIORITY_SCALING_PARAM;
+                priority = (min_distance + min_distance2)*10; // agent-box-goal sum distance x10
+                Task task1 = new Task(taskId,matchedAgent.getId(),matchedBox.getLocation(),priority);
+                taskId++;
+                //task2 is to push/pull box to goal.
+                //TODO: When planning task2, need to check if agent is beside the box. Otherwise create a new task of task1 to let agent find box.
+                Task task2 = new Task(taskId,matchedAgent.getId(),matchedBox.getId(), goal.getLocation(),priority-1);
+                taskId++;
+                data.addTask(task1);
+                data.addTask(task2);
+//                goalCount--;
+            }
+
+
+        }
+        System.err.println("[TaskHandler] Initial tasks: "+data.getAllTasks().toString());
     }
 
     public void assignTask(){
@@ -44,25 +134,30 @@ public class TaskHandler {
         for (Goal goal : allGoals.values()){
             //for each goal, find a same name box. Currently use the first matching box.
             ArrayList<Integer> boxList = data.getBoxByName(goal.getName());
+            // TODO: box-goal matching
+            if(boxList.size()!=0 && boxList != null){ // if there is any box
+                Box box = allBoxes.get(boxList.get(0)); //TODO: only one box? improve goal-box matching. Remove box after pairing
+                //then for the box, find a same color agent. Currently use the first matching agent.
+                ArrayList<Integer> agentList = data.getAgentByColor(box.getColor());
+                Agent agent = allAgents.get(agentList.get(0));//TODO: improve box-agent matching
 
-            Box box = allBoxes.get(boxList.get(0)); //TODO: improve goal-box matching. Remove box after pairing
+                //for each goal, create 2 tasks. Tasks of the first goal has highest priority.
+                //task1 is to find box. Task1 has higher priority than task2.
+                priority = goalCount*PRIORITY_SCALING_PARAM;
+                Task task1 = new Task(taskId,agent.getId(),box.getLocation(),priority);
+                taskId++;
+                //task2 is to push/pull box to goal.
+                //TODO: When planning task2, need to check if agent is beside the box. Otherwise create a new task of task1 to let agent find box.
+                Task task2 = new Task(taskId,agent.getId(),box.getId(), goal.getLocation(),priority-1);
+                taskId++;
+                data.addTask(task1);
+                data.addTask(task2);
+                goalCount--;
+            }else { // No box
+                //TODO: agent to goal
 
-            //then for the box, find a same color agent. Currently use the first matching agent.
-            ArrayList<Integer> agentList = data.getAgentByColor(box.getColor());
-            Agent agent = allAgents.get(agentList.get(0));//TODO: improve box-agent matching
+            }
 
-            //for each goal, create 2 tasks. Tasks of the first goal has highest priority.
-            //task1 is to find box. Task1 has higher priority than task2.
-            priority = goalCount*PRIORITY_SCALING_PARAM;
-            Task task1 = new Task(taskId,agent.getId(),box.getLocation(),priority);
-            taskId++;
-            //task2 is to push/pull box to goal.
-            //TODO: When planning task2, need to check if agent is beside the box. Otherwise create a new task of task1 to let agent find box.
-            Task task2 = new Task(taskId,agent.getId(),box.getId(), goal.getLocation(),priority-1);
-            taskId++;
-            data.addTask(task1);
-            data.addTask(task2);
-            goalCount--;
 
         }
         System.err.println("[TaskHandler] Initial tasks: "+data.getAllTasks().toString());
